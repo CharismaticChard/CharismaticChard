ba4ed22e6d617986f6fd4f4895f02259114208d4
Minji Lee <minjilee8816@gmail.com>
2017-07-14 19:40:34 -0700

    replaced the original client files to aviod conflicts

--- client/src/app.js
+++ client/src/app.js
@@ -1,32 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 
-
-class App extends React.Component {
-  constructor (props) {
-    super(props); 
-  }
-
-  buttonClick() {
-    axios.post('/twilio', sampleDataFormatFromClient)
-      .then( function (response) {
-        console.log(response);
-      })
-      .catch( function (response) {
-        console.log(response);
-      });
-  }
-
-
-  render() {
-    return (
-      <div>
-        <input onClick={this.buttonClick.bind(this)} type="button" value="submit!"/>
-      </div>
-    );
-  }
-}
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>Hello World from React</h1>, document.getElementById('root'));
