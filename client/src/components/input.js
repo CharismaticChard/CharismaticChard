da35000f1522ea7667e78ff01e4bec733c8355a1
Kai Mashima <kaiomashima@gmail.com>
2017-07-15 15:11:27 -0700

    (feat) adds code to change head file to reflect new css file location updates input component

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/lib/Navbar';
 import Button from 'react-bootstrap/lib/Button';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import FormControl from 'react-bootstrap/lib/FormControl';
@@ -17,23 +16,47 @@ const Input = ({numbers}) => {
     <div>
       <h1>Hello Input</h1>
       <p>{numbers}</p>
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <h1>Input Receipt Items</h1>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <hr/>
-        <Navbar.Collapse>
-          <Navbar.Form>
-            <FormGroup>
-              <FormControl type="text" placeholder="Item Name..." />
-              <FormControl type="number" placeholder="Price..." />
-            </FormGroup>
-            <Button type="submit">Submit</Button>
-          </Navbar.Form>
-        </Navbar.Collapse>
-      </Navbar>
+      <div className="inputContainer">
+        <div className="inputHeader">
+          <h1>Input Receipt Items</h1>
+          <hr/>
+        </div>
+        <div className="inputBody">
+          <FormGroup>
+            <div className="formContainer">
+              <div className="formItem">
+                <FormControl className="formItemBit" type="text" placeholder="Item Name..." />
+                <FormControl className="formItemBit" type="number" placeholder="Price..." />
+              </div>
+              <div className="formItem">
+                <FormControl className="formItemBit" type="text" placeholder="Item Name..." />
+                <FormControl className="formItemBit" type="number" placeholder="Price..." />
+              </div>
+              <div className="formItem">
+                <FormControl className="formItemBit" type="text" placeholder="Item Name..." />
+                <FormControl className="formItemBit" type="number" placeholder="Price..." />
+              </div>
+            </div>
+            <hr/>
+          </FormGroup>
+          <br></br>
+          <FormGroup>
+            <div className="formContainer">
+              <div className="formItem">
+                <FormControl type="number" placeholder="Tax..." />
+              </div>
+              <div className="formItem">
+                <FormControl type="number" placeholder="Total..." />
+              </div>
+              <div className="formItem">
+                <FormControl type="number" placeholder="Tip..." />
+              </div>
+            </div>
+            <hr/>
+          </FormGroup>
+          <Button type="submit">Submit</Button>
+        </div>
+      </div>
     </div>
   );
 };
