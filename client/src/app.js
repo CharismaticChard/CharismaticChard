9b9d3c6efe61d7687fcb3aca595a3ac8c581e32d
Minji Lee <minjilee8816@gmail.com>
2017-07-14 19:16:15 -0700

    lanched text messages function successfuly

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-ReactDOM.render(<h1>Hello World from React</h1>, document.getElementById('root'));
+
+class App extends React.Component {
+  constructor(props) {
+    super(props); 
+  }
+
+  buttonClick() {
+    console.log('checking!')
+
+    var sampleDataFormatFromClient = {
+      billTotal: 100,
+      owner : {
+          name: 'Minji', 
+          phone: '+15108290026', 
+          debtTotal: '$13',
+          item : [ 
+            { itemName : 'pizza',
+              itemPrice : '$10',
+              tax : '$1',
+              tip : '$2'
+            } 
+          ]
+      }, 
+      debtors: [
+          {
+            name: 'Kai', 
+            phone: '+16508155855', 
+            debtTotal: '$16',
+            item : [ 
+              { itemName : 'salad',
+                itemPrice : '$11'
+              }, 
+            ],
+            tax : '$2',
+            tip : '$3'
+          },
+          
+          {
+            name: 'Carlos', 
+            phone: '+14433109844', 
+            debtTotal: '$20',
+            item : [ 
+              { itemName : 'tacos',
+                itemPrice : '$15'
+              } 
+            ],
+            tax: '$2',
+            tip: '$3'
+          },
+    
+          {
+            name: 'Joe', 
+            phone: '+17146844358', 
+            debtTotal: '$23',
+            item : [ 
+              { itemName : 'burger',
+                itemPrice : '$18'
+              }
+            ],
+            tax : '$2',
+            tip : '$3'
+          }
+      ],
+      billName: 'thesis Group'
+    }
+
+    axios.post('/twilio', sampleDataFormatFromClient)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (response) {
+      console.log(response);
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input onClick={this.buttonClick.bind(this)} type="button" value="submit!"/>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 'use strict';
 const app = require('./app');
 const db = require('../db');
+var express = require('express');
+var rp = require('request-promise');
+var bodyParser = require('body-parser');
+
+var twilioMiddleware = require('./twilio/twilio_middleware.js')
+
 const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => {
   console.log('Example app listening on port 3000!');
 });
+
+
+app.post('/twilio', twilioMiddleware.twilioMiddleware )
+
+
+
