91afac11d510584d8493a1e0bbd62d8cbb23f5fc
Minji Lee <minjilee8816@gmail.com>
2017-07-15 15:13:51 -0700

    created output components

---
+++ client/src/components/output.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Button from 'react-bootstrap/lib/Button';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import { connect } from 'react-redux';
+
+
+var dummydata= {
+
+}
+
+
+
+class Output extends React.Component {
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Bill</h1>
+        <table>
+          
+        </table>
+      </div>
+    )
+  }
+}
+
+export default Output;
