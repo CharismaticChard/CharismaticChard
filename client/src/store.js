fcc594b164fd9c756f010bceebf4f4a9fb4f713c
Kai Mashima <kaiomashima@gmail.com>
2017-07-18 15:22:30 -0700

    (bug) adds code to remove react-redux-form

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
