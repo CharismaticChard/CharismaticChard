52d2ad79b83cb3633c3d027bad5386b7257a8972
Kai Mashima <kaiomashima@gmail.com>
2017-07-18 15:27:12 -0700

    (bug) adds fixes to removed react-redux-form

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
