02767fb5ece6e45990be220d54a68ba406ef13ab
Kai Mashima <kaiomashima@gmail.com>
2017-07-20 11:02:08 -0700

    (bug) adds code to switch to master

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import numbers from './testReducer.js';
 import input from './inputReducer.js';
 import output from './outputReducer.js';
+import final from './finalReducer.js';
 
 const reducer = combineReducers({
   numbers,
