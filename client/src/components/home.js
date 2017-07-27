11b261be18a299195ab3d094b4db81a2a3230a68
Minji Lee <minjilee8816@gmail.com>
2017-07-27 12:17:40 -0700

    about to rebase and clean up the code

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 
-
 class Home extends React.Component {
   render() {
     return (
