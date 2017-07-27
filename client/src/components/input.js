15d80f1935e0be75e18e21802cfd51befcd5eb06
Minji <minjilee8816@gmail.com>
2017-07-26 17:37:18 -0700

    Update input.js

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -3,9 +3,9 @@ import Button from 'react-bootstrap/lib/Button';
 import { connect } from 'react-redux';
 import $ from 'jquery';
 import { Link } from 'react-router-dom';
-import { setIterator, removeIterator, setItems, setTax, setTotal, setTip, inputRedirect } from '../actions/inputActions.js';
+import { setIterator, removeIterator, setItems, setTax, setTotal, setTip } from '../actions/inputActions.js';
 import { setSplitName } from '../actions/finalActions.js';
-import { LinkContainer } from 'react-router-bootstrap';
+
 const mapStateToProps = state => {
   return {
     iterator: state.input.iterator,
@@ -34,9 +34,6 @@ const mapDispatchToProps = dispatch => {
     ),
     setSplitName: (input) => dispatch(
       setSplitName(input)
-    ),
-    inputRedirect: (input) => dispatch(
-      inputRedirect(input)
     ),
   };
 };
@@ -68,7 +65,6 @@ class Input extends React.Component {
     $('.total').val('');
     p.setTip($('.tip').val());
     $('.tip').val('');
-    this.props.inputRedirect(true);
   }
 
   addItem() {
@@ -85,6 +81,14 @@ class Input extends React.Component {
   render() {
     return (
       <div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="logo text-center">
+              <img src="./assets/splitter-logo.png" className="mx-auto d-block" width="200"/>
+            </div>
+          </div>
+        </div>
+        <hr />
         <div className="container-fluid">
           <div className="inputContainer row formItem">
             <div className="inputItem col-md-4">
@@ -131,11 +135,7 @@ class Input extends React.Component {
             </div>
             <footer>
               <hr className="footerHR"/>
-              <LinkContainer to="/output" >
-                <Button className="btn btn-primary" onClick={this.handleSubmit.bind(this)}>
-                  Submit
-                </Button>
-              </LinkContainer>
+              <Link className="btn btn-primary" to="/output" onClick={this.handleSubmit.bind(this)}>Submit</Link>
             </footer>
           </div>
         </div>
