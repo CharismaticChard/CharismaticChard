62d7ebf52bbee0ba7e83d9676ac8da43b06de6da
Minji Lee <minjilee8816@gmail.com>
2017-07-29 15:15:05 -0700

    found out the position of div box

--- client/src/components/addImage.js
+++ client/src/components/addImage.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import $ from 'jquery';
 import Button from 'react-bootstrap/lib/Button';
-// import Draggable from 'react-draggable';
-// import ResizableBox from 'react-resizable-component';
 import Rnd from 'react-rnd';
 
 const mapStateToProps = state => {
@@ -23,11 +21,17 @@ class AddImage extends React.Component {
     super(props);
     this.state = {
       file: '',
-      imagePreviewURL: ''
+      imagePreviewURL: '',
+      dimensions: {},
+      firstX: null,
+      firstY: null,
+      secondX: null,
+      secondY: null
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.dragDiv = this.dragDiv.bind(this);
+    this.selectedPosition = this.selectedPosition.bind(this);
+    this.imageOnLoad = this.imageOnLoad.bind(this);
   }
 
   handleChange(e) {
@@ -43,36 +47,62 @@ class AddImage extends React.Component {
     reader.readAsDataURL(file);
   }
 
-  dragDiv () {
+  selectedPosition () {
+    let link = $('.item-selection');
+    let offset = link.offset();
+    let top = Number(offset.top);
+    let left = Number(offset.left);
+    let right = link.css('width');
+    let bottom = link.css('height');
+    right = Number(right.slice(0, right.length-3)) + left;
+    bottom = Number(bottom.slice(0, bottom.length-3)) + top;
+    let position = $('.item-selection').position();
+    this.setState({
+      firstX: left,
+      firstY: top,
+      secondX: right,
+      secondY: bottom
+    });
+    console.log('X: ' + left + ', Y: ' + top );
+    console.log( 'right: ' + right+ ', bottom: ' + bottom );
+  }
 
+  imageOnLoad ({target:img}) {
+    this.setState({
+      dimensions: {
+        height: img.offsetHeight,
+        width: img.offsetWidth
+      }
+    });
   }
 
   render() {
-    var image = (
+    let image = (
       <div className="col-xs-11 previewImageContainer text-center">
         <p>Take Picture to Continue</p>
       </div>
     );
     
-    var { imagePreviewURL } = this.state;
-
+    let { imagePreviewURL } = this.state;
+    // console.log('imagePreviewURL', imagePreviewURL)
     if (imagePreviewURL) {
       image = (
         <div className="previewImageContainer">
-
-
           <Rnd
             default={{
-              x: 0,
+              x: 50,
               y: 0,
-              width: 320,
-              height: 200,
+              width: 200,
+              height: 50,
             }}
-            className="item-selection">
-            You can resize the box
+            className="item-selection"
+            onDragStop={this.selectedPosition}
+            onResizeStop={this.selectedPosition}>
           </Rnd>
-          <img className="previewImage" src={imagePreviewURL}/>
-          <Button className="col-xs-2"> 
+
+          <img className="previewImage" src={imagePreviewURL} onLoad={this.imageOnLoad}/>
+          {console.log(this.state.dimensions)}
+          <Button className="col-xs-2" > 
             Select
           </Button>
         </div>
@@ -82,7 +112,7 @@ class AddImage extends React.Component {
     return (
       <div>
         <div className="container-fluid">
-          <div className="row">
+          <div className="row previewImageContainer">
             <div className="col-xs-12">
               <input className="form-control" type="file" accept="image/*" capture="camera" id="camera" placeholder="Take Picture" onChange={this.handleChange} />
             </div>
