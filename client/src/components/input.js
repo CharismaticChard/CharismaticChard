504ae260ba47e399f3263594f6d2d391c718dfb4
Kai Mashima <kaiomashima@gmail.com>
2017-08-02 15:53:29 -0700

    (refactor) adds code to completely overhaul input.js file, dumping jquery

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -4,45 +4,134 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { inputLoading } from '../actions/historyAction.js';
-import ItemInputList from './itemInputList.js';
-import ItemEditList from './itemEditList.js';
+import { addItem, removeItem, setItem, setTax, setTotal, setTip, setSplitName } from '../actions/inputActions.js';
 
 const mapStateToProps = state => {
   return {
     items: state.input.items,
+    tax: state.input.tax,
+    total: state.input.total,
+    tip: state.input.tip,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
+    addItem: (input) => dispatch(
+      addItem(input)
+    ),
+    removeItem: () => dispatch(
+      removeItem()
+    ),
+    setItem: (input, index) => dispatch(
+      setItem(input, index)
+    ),
+    setTax: (input) => dispatch(
+      setTax(input)
+    ),
+    setTotal: (input) => dispatch(
+      setTotal(input)
+    ),
+    setTip: (input) => dispatch(
+      setTip(input)
+    ),
+    setSplitName: (input) => dispatch(
+      setSplitName(input)
+    ),
   };
 };
 
 class Input extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      counter: 1,
-    };
+    this.handleChangeDyna = this.handleChangeDyna.bind(this);
+    this.handleChangeTax = this.handleChangeTax.bind(this);
+    this.handleChangeTotal = this.handleChangeTotal.bind(this);
+    this.handleChangeTip = this.handleChangeTip.bind(this);
+  }
+
+  handleChangeName(e) {
+    e.preventDefault();
+    this.props.setSplitName(e.target.value);
+  }
+
+  handleChangeDyna(e) {
+    e.preventDefault();
+    var val = e.target.value;
+    var index = e.target.id;
+    var obj = this.props.items.slice()[index];
+    if (e.target.type === 'number') {
+      obj.price = Number(val);
+    } else if (e.target.type === 'text') {
+      obj.item = val;
+    }
+    this.props.setItem(obj, index);
+  }
+
+  handleChangeTax(e) {
+    e.preventDefault();
+    this.props.setTax(Number(e.target.value));
+  }
+
+  handleChangeTotal(e) {
+    e.preventDefault();
+    this.props.setTotal(Number(e.target.value));
+  }
+
+  handleChangeTip(e) {
+    e.preventDefault();
+    this.props.setTip(Number(e.target.value));
   }
 
   addItem() {
-    this.setState({
-      counter: this.state.counter + 1
-    });
+    this.props.addItem({item: undefined, price: undefined});
   }
 
   removeItem() {
-    this.setState({
-      counter: this.state.counter - 1
-    });
+    this.props.removeItem();
   }
 
   render() {
-    var itemList = (this.props.items.length === 0) ? <ItemInputList counter={this.state.counter}/> : <ItemEditList />;
     return (
       <div className="container">
-        {itemList}
+        <div>
+          <div className="inputContainer row formItem">
+            <div className="inputItem col-xs-12">
+              <label className="inputItemBit">Split Name</label>
+              <input type="text" className="inputItemBit name form-control" placeholder="Name..." required/>
+            </div>
+          </div>
+          <div className="items">
+            {
+              this.props.items.map((item, index) => (
+                <div key={index} className="inputContainer row formItem">
+                  <div className="inputItem col-xs-6">
+                    <label className="inputItemBit">Item</label>
+                    <input type="text" className="inputItemBit form-control" id={index} placeholder="Item..." value={item.item} onChange={this.handleChangeDyna} required/>
+                  </div>
+                  <div className="inputItem col-xs-6">
+                    <label className="inputItemBit">Price</label>
+                    <input type="number" className="inputItemBit form-control" id={index} placeholder="Price..." value={item.price} onChange={this.handleChangeDyna} required/>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+          <div className="inputContainer row formItem">
+            <div className="inputItem col-xs-12">
+              <label className="inputItemBit">Tax</label>
+              <input type="number" className="inputItemBit tax form-control" placeholder="Tax..." value={this.props.tax} onChange={this.handleChangeTax} required/>
+            </div>
+            <div className="inputItem col-xs-12">
+              <label className="inputItemBit">Total</label>
+              <input type="number" className="inputItemBit total form-control" placeholder="Total..." value={this.props.total} onChange={this.handleChangeTotal} required/>
+            </div>
+            <div className="inputItem col-xs-12">
+              <label className="inputItemBit">Tip</label>
+              <input type="number" className="inputItemBit tip form-control" placeholder="Tip..." value={this.props.tip} onChange={this.handleChangeTip} required/>
+            </div>
+          </div>
+        </div>
         <div className="inputContainer row formItem">
           <div className="inputItem col-xs-6">
             <Button className="btn btn-sm btn-primary" onClick={this.addItem.bind(this)}>Add Item</Button>
